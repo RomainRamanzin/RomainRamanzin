f8bc1b41c7f5a8d1301e79e85c12347e4d0f7914
Romain Ramanzin <romain.ramanzin@gmail.com>
2024-05-28 13:16:51 +0200

    Fix - index regeneration

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const randomNumber = Math.floor(Math.random() * 100);
 if (randomNumber > 60) {
   var readmeContent = readme.replace('<#today_date>', today);
   readmeContent += `\n<!-- Last updated: ${timestamp} -->`;
+
+  // Write the new README.md file
+  fs.writeFile('README.md', readmeContent, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
 } else {
   console.log('Skipping readme upadate');
 }
-
-
-
-fs.writeFile('README.md', readmeContent, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
